63942953f64ed045ad7c4d5c78da612c778b4570
Kickassh <kickassh13@gmail.com>
2025-09-22 19:47:10 +0530

    Updates

---
+++ assets/index-raoW03q8.js
@@ -0,0 +1,134 @@
+(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const c of e)if(c.type==="childList")for(const r of c.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function o(e){const c={};return e.integrity&&(c.integrity=e.integrity),e.referrerPolicy&&(c.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?c.credentials="include":e.crossOrigin==="anonymous"?c.credentials="omit":c.credentials="same-origin",c}function n(e){if(e.ep)return;e.ep=!0;const c=o(e);fetch(e.href,c)}})();/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const v=([t,a,o])=>{const n=document.createElementNS("http://www.w3.org/2000/svg",t);return Object.keys(a).forEach(e=>{n.setAttribute(e,String(a[e]))}),o!=null&&o.length&&o.forEach(e=>{const c=v(e);n.appendChild(c)}),n},A=(t,a={})=>{const o="svg",n={...y,...a};return v([o,n,t])};/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=t=>Array.from(t.attributes).reduce((a,o)=>(a[o.name]=o.value,a),{}),k=t=>typeof t=="string"?t:!t||!t.class?"":t.class&&typeof t.class=="string"?t.class.split(" "):t.class&&Array.isArray(t.class)?t.class:"",C=t=>t.flatMap(k).map(o=>o.trim()).filter(Boolean).filter((o,n,e)=>e.indexOf(o)===n).join(" "),I=t=>t.replace(/(\w)(\w*)(_|-|\s*)/g,(a,o,n)=>o.toUpperCase()+n.toLowerCase()),g=(t,{nameAttr:a,icons:o,attrs:n})=>{var h;const e=t.getAttribute(a);if(e==null)return;const c=I(e),r=o[c];if(!r)return;const l=x(t),s={...y,"data-lucide":e,...n,...l},i=C(["lucide",`lucide-${e}`,l,n]);i&&Object.assign(s,{class:i});const p=A(r,s);return(h=t.parentNode)==null?void 0:h.replaceChild(p,t)};/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const S=[["path",{d:"M5 12h14"}],["path",{d:"m12 5 7 7-7 7"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const O=[["path",{d:"M3 3v16a2 2 0 0 0 2 2h16"}],["path",{d:"m19 9-5 5-4-4-3 3"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=[["path",{d:"m6 9 6 6 6-6"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=[["path",{d:"M12 6v6l4 2"}],["circle",{cx:"12",cy:"12",r:"10"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=[["path",{d:"m18 16 4-4-4-4"}],["path",{d:"m6 8-4 4 4 4"}],["path",{d:"m14.5 4-5 16"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const T=[["path",{d:"M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["path",{d:"M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"}],["path",{d:"M22 10v6"}],["path",{d:"M6 12.5V16a6 3 0 0 0 12 0v-3.5"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const q=[["rect",{width:"20",height:"20",x:"2",y:"2",rx:"5",ry:"5"}],["path",{d:"M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"}],["line",{x1:"17.5",x2:"17.51",y1:"6.5",y2:"6.5"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const z=[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"}],["rect",{width:"4",height:"12",x:"2",y:"9"}],["circle",{cx:"4",cy:"4",r:"2"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=[["path",{d:"m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"}],["rect",{x:"2",y:"4",width:"20",height:"16",rx:"2"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const V=[["path",{d:"M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"}],["circle",{cx:"12",cy:"10",r:"3"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const $=[["path",{d:"M4 5h16"}],["path",{d:"M4 12h16"}],["path",{d:"M4 19h16"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=[["path",{d:"M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const D=[["path",{d:"M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const F=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"}],["path",{d:"m21.854 2.147-10.94 10.939"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const G=[["path",{d:"M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=[["circle",{cx:"12",cy:"12",r:"4"}],["path",{d:"M12 2v2"}],["path",{d:"M12 20v2"}],["path",{d:"m4.93 4.93 1.41 1.41"}],["path",{d:"m17.66 17.66 1.41 1.41"}],["path",{d:"M2 12h2"}],["path",{d:"M20 12h2"}],["path",{d:"m6.34 17.66-1.41 1.41"}],["path",{d:"m19.07 4.93-1.41 1.41"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const R=[["rect",{width:"10",height:"14",x:"3",y:"8",rx:"2"}],["path",{d:"M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4"}],["path",{d:"M8 18h.01"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const U=[["path",{d:"M12 19h8"}],["path",{d:"m4 17 6-6-6-6"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const X=[["path",{d:"M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const K=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87"}],["circle",{cx:"9",cy:"7",r:"4"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _=[["path",{d:"M18 6 6 18"}],["path",{d:"m6 6 12 12"}]];/**
+ * @license lucide v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=({icons:t={},nameAttr:a="data-lucide",attrs:o={},root:n=document}={})=>{if(!Object.values(t).length)throw new Error(`Please provide an icons object.
+If you want to use all the icons you can import it like:
+ \`import { createIcons, icons } from 'lucide';
+lucide.createIcons({icons});\``);if(typeof n>"u")throw new Error("`createIcons()` only works in a browser environment.");const e=n.querySelectorAll(`[${a}]`);if(Array.from(e).forEach(c=>g(c,{nameAttr:a,icons:t,attrs:o})),a==="data-lucide"){const c=n.querySelectorAll("[icon-name]");c.length>0&&Array.from(c).forEach(r=>g(r,{nameAttr:"icon-name",icons:t,attrs:o}))}},f=document.documentElement,u=document.getElementById("theme-toggle-desktop"),m=document.getElementById("theme-toggle"),d=(t,a)=>{t&&(t.innerHTML=`<i data-lucide="${a}"></i>`,b({icons:{Sun:w,Moon:M}}))},L=()=>{f.classList.contains("dark")?(f.classList.remove("dark"),d(u,"moon"),d(m,"moon")):(f.classList.add("dark"),d(u,"sun"),d(m,"sun"))};u&&u.addEventListener("click",L);m&&m.addEventListener("click",L);const E=f.classList.contains("dark")?"sun":"moon";d(u,E);d(m,E);document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll('a[href^="#"]').forEach(s=>{s.addEventListener("click",function(i){i.preventDefault();const p=this.getAttribute("href"),h=document.querySelector(p);h&&(h.scrollIntoView({behavior:"smooth",block:"start"}),history.pushState(null,"",p))})});const t={threshold:.1,rootMargin:"0px 0px -50px 0px"},a=new IntersectionObserver(s=>{s.forEach(i=>{i.isIntersecting&&i.target.classList.add("animate-fade-in")})},t);document.querySelectorAll("section").forEach(s=>{a.observe(s)});const o=document.getElementById("hamburger-menu-cta"),n=document.getElementById("mobile-menu"),e=document.getElementById("mobile-menu-overlay"),c=document.getElementById("mobile-menu-close"),r=()=>{n&&(n.classList.remove("-translate-y-full"),n.setAttribute("aria-hidden","false"),e&&e.classList.remove("hidden"))},l=()=>{n&&(n.classList.add("-translate-y-full"),n.setAttribute("aria-hidden","true"),e&&e.classList.add("hidden"))};o&&o.addEventListener("click",r),c&&c.addEventListener("click",l),e&&e.addEventListener("click",l),document.querySelectorAll('#mobile-menu a[href^="#"]').forEach(s=>{s.addEventListener("click",()=>l())})});b({icons:{Moon:M,GraduationCap:j,Menu:$,ArrowRight:S,CodeXml:P,Star:G,Clock:B,Users:K,ChartLine:O,TabletSmartphone:R,ChevronDown:N,Terminal:U,Phone:D,Mail:H,MapPin:V,Send:F,Facebook:T,Twitter:X,Instagram:q,Linkedin:z,Sun:w,X:_}});
